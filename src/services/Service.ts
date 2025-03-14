b1e0692d5ab665316065f17233d8c3b60ce470b5
Nathan <ribeiro.nathan2002@gmail.com>
2025-03-14 16:27:15 -0300

    Deploy do Projeto

--- src/services/Service.ts
+++ src/services/Service.ts
@@ -1,11 +1,15 @@
 import axios from "axios";
 
-const BASE_URL = "https://servicodados.ibge.gov.br/api/v3/agregados";
+// Criando a instância do Axios com a base URL correta
+export const api = axios.create({
+    baseURL: import.meta.env.VITE_API_URL // Removido o ponto e vírgula
+});
 
+// Função para buscar dados do PIB
 export const getPibData = async () => {
     try {
-        const response = await axios.get(
-            `${BASE_URL}/5938/periodos/-100/variaveis/37?localidades=N1[all]`
+        const response = await api.get(
+            "/5938/periodos/-100/variaveis/37?localidades=N1[all]"
         );
         return response.data;
     } catch (error) {
@@ -14,10 +18,11 @@ export const getPibData = async () => {
     }
 };
 
+// Função para buscar dados da população
 export const getPopulationData = async () => {
     try {
-        const response = await axios.get(
-            `${BASE_URL}/6579/periodos/-100/variaveis/9324?localidades=N1[all]`
+        const response = await api.get(
+            "/6579/periodos/-100/variaveis/9324?localidades=N1[all]"
         );
         return response.data;
     } catch (error) {
